5a8b350e7b1ec62d003dae9026a1a1e165bb76cf
spicyzboss <73651621+spicyzboss@users.noreply.github.com>
2022-04-13 06:29:34 +0700

    test: express listener

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+// import pclient from '@prisma/client';
+import express from 'express';
+import clc from 'cli-color';
+import { config } from 'dotenv';
 
-export default async function seed() {
-  const prisma = new PrismaClient();
-}
+config();
+
+// const { PrismaClient } = pclient;
+
+const PORT = process.env.PORT || 3001;
+// const prisma = new PrismaClient();
+const app = express();
+
+const listener = app.listen(PORT, () => {
+  process.stdout.write(`${clc.greenBright(`Started on port ${clc.yellow(listener.address().port)}`)}\n`);
+});
