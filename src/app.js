9f8bf6e77811472d6b55519d6b3573004d684eff
spicyzboss <73651621+spicyzboss@users.noreply.github.com>
2022-04-28 05:06:21 +0700

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { config } from 'dotenv';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { register, login, mailVerification } from './services';
 
 config();
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/login', login);
