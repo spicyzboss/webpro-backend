49915651b28783d29df95732c152a6f2585099aa
Trda092 <73659917+Trda092@users.noreply.github.com>
2022-05-05 17:31:15 +0700

    post edit

--- src/services/post.js
+++ src/services/post.js
@@ -42,12 +42,10 @@ const createPost = async (req, res) => {
 };
 
 const findPost = async (req, res) => {
-  const { interest } = req.body;
-  const interestName = interest.map((obj) => obj.name[0].toUpperCase() + obj.name.substr(1).toLowerCase());
-  console.log(interestName)
+  const { name } = req.body;
   const interestitem = await prisma.interest.findMany({
     where: {
-      name: { in: interestName },
+      name,
     },
   });
   const selectedInterestID = interestitem.map((its) => its.id);
